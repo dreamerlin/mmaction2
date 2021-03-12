b0eb34d78ca38df5cf370612fcedbd08c9ce1751
dreamerlin <528557675@qq.com>
2021-03-13 02:03:43 +0800

    gst head

--- mmaction/models/heads/gst_head.py
+++ mmaction/models/heads/gst_head.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+from mmcv.cnn import normal_init
 
 from ..registry import HEADS
 from .base import BaseHead
@@ -12,26 +13,36 @@ class GSTHead(BaseHead):
                  in_channels,
                  loss_cls=dict(type='CrossEntropyLoss'),
                  spatial_type='avg',
+                 dropout_ratio=0.3,
+                 init_std=0.001,
                  **kwargs):
         super().__init__(num_classes, in_channels, loss_cls, **kwargs)
 
         self.spatial_type = spatial_type
+        self.dropout_ratio = dropout_ratio
+        self.init_std = init_std
+        if self.dropout_ratio != 0:
+            self.dropout = nn.Dropout(p=self.dropout_ratio)
+        else:
+            self.dropout = None
         self.fc_cls = nn.Linear(self.in_channels, self.num_classes)
 
         if self.spatial_type == 'avg':
-            # use `nn.AdaptiveAvgPool3d` to adaptively match the in_channels.
+            # use `nn.AdaptiveAvgPool2d` to adaptively match the in_channels.
             self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
         else:
             self.avg_pool = None
 
     def init_weights(self):
-        pass
+        normal_init(self.fc_cls, std=self.init_std)
 
     def forward(self, x):
         x = x.transpose(1, 2).contiguous()
         x = x.view((-1, ) + x.size()[2:])
         if self.avg_pool is not None:
             x = self.avg_pool(x)
+        if self.dropout is not None:
+            x = self.dropout(x)
         x = x.view(x.size(0), -1)
         x = self.fc(x)
 
