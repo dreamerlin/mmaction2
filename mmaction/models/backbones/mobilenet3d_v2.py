5cd9a19811c582209b2517556760b47cf9454bda
dreamerlin <528557675@qq.com>
2021-03-28 19:57:06 +0800

    mobilenet3d inflate

--- mmaction/models/backbones/mobilenet3d_v2.py
+++ mmaction/models/backbones/mobilenet3d_v2.py
@@ -1,11 +1,11 @@
 import torch.nn as nn
 from mmcv.cnn import ConvModule, constant_init, kaiming_init
-from mmcv.runner import load_checkpoint
+from mmcv.runner import _load_checkpoint, load_checkpoint
 from torch.nn.modules.batchnorm import _BatchNorm
 
 from ...utils import get_root_logger
 from ..builder import BACKBONES
-from .mobilenet_v2 import MobileNetV2
+from .mobilenet_v2 import MobileNetV2, make_divisible
 
 
 @BACKBONES.register_module()
@@ -17,8 +17,8 @@ class MobileNet3DV2(MobileNetV2):
                  conv_cfg=dict(type='Conv3d'),
                  norm_cfg=dict(type='BN3d', requires_grad=True),
                  **kwargs):
-        super(MobileNetV2, self).__init__(
-            *args, conv_cfg=conv_cfg, norm_cfg=norm_cfg, **kwargs)
+        super().__init__(*args, conv_cfg=conv_cfg, norm_cfg=norm_cfg, **kwargs)
+        self.in_channels = make_divisible(32 * self.widen_factor, 8)
         self.conv1 = ConvModule(
             in_channels=3,
             out_channels=self.in_channels,
@@ -30,13 +30,112 @@ class MobileNet3DV2(MobileNetV2):
             act_cfg=self.act_cfg)
         self.pretrained2d = pretrained2d
 
+    @staticmethod
+    def _inflate_conv_params(conv3d, state_dict_2d, module_name_2d,
+                             inflated_param_names):
+        """Inflate a conv module from 2d to 3d.
+
+        Args:
+            conv3d (nn.Module): The destination conv3d module.
+            state_dict_2d (OrderedDict): The state dict of pretrained 2d model.
+            module_name_2d (str): The name of corresponding conv module in the
+                2d model.
+            inflated_param_names (list[str]): List of parameters that have been
+                inflated.
+        """
+        weight_2d_name = module_name_2d + '.weight'
+
+        conv2d_weight = state_dict_2d[weight_2d_name]
+        kernel_t = conv3d.weight.data.shape[2]
+
+        new_weight = conv2d_weight.data.unsqueeze(2).expand_as(
+            conv3d.weight) / kernel_t
+        conv3d.weight.data.copy_(new_weight)
+        inflated_param_names.append(weight_2d_name)
+
+        if getattr(conv3d, 'bias') is not None:
+            bias_2d_name = module_name_2d + '.bias'
+            conv3d.bias.data.copy_(state_dict_2d[bias_2d_name])
+            inflated_param_names.append(bias_2d_name)
+
+    @staticmethod
+    def _inflate_bn_params(bn3d, state_dict_2d, module_name_2d,
+                           inflated_param_names):
+        """Inflate a norm module from 2d to 3d.
+
+        Args:
+            bn3d (nn.Module): The destination bn3d module.
+            state_dict_2d (OrderedDict): The state dict of pretrained 2d model.
+            module_name_2d (str): The name of corresponding bn module in the
+                2d model.
+            inflated_param_names (list[str]): List of parameters that have been
+                inflated.
+        """
+        for param_name, param in bn3d.named_parameters():
+            param_2d_name = f'{module_name_2d}.{param_name}'
+            param_2d = state_dict_2d[param_2d_name]
+            param.data.copy_(param_2d)
+            inflated_param_names.append(param_2d_name)
+
+        for param_name, param in bn3d.named_buffers():
+            param_2d_name = f'{module_name_2d}.{param_name}'
+            # some buffers like num_batches_tracked may not exist in old
+            # checkpoints
+            if param_2d_name in state_dict_2d:
+                param_2d = state_dict_2d[param_2d_name]
+                param.data.copy_(param_2d)
+                inflated_param_names.append(param_2d_name)
+
+    def _inflate_weights(self, logger):
+        state_dict_r2d = _load_checkpoint(self.pretrained)
+        if 'state_dict' in state_dict_r2d:
+            state_dict_r2d = state_dict_r2d['state_dict']
+
+        inflated_param_names = []
+        for name, module in self.named_modules():
+            if isinstance(module, ConvModule):
+                original_conv_name = name + '.conv'
+                original_bn_name = name + '.bn'
+
+                if original_conv_name + '.weight' not in state_dict_r2d:
+                    logger.warning(f'Module not exist in the state_dict_r2d'
+                                   f': {original_conv_name}')
+                else:
+                    shape_2d = state_dict_r2d[original_conv_name +
+                                              '.weight'].shape
+                    shape_3d = module.conv.weight.data.shape
+                    if shape_2d != shape_3d[:2] + shape_3d[3:]:
+                        logger.warning(f'Weight shape mismatch for '
+                                       f': {original_conv_name} : '
+                                       f'3d weight shape: {shape_3d}; '
+                                       f'2d weight shape: {shape_2d}. ')
+                    else:
+                        self._inflate_conv_params(module.conv, state_dict_r2d,
+                                                  original_conv_name,
+                                                  inflated_param_names)
+
+                if original_bn_name + '.weight' not in state_dict_r2d:
+                    logger.warning(f'Module not exist in the state_dict_r2d'
+                                   f': {original_bn_name}')
+                else:
+                    self._inflate_bn_params(module.bn, state_dict_r2d,
+                                            original_bn_name,
+                                            inflated_param_names)
+
+        # check if any parameters in the 2d checkpoint are not loaded
+        remaining_names = set(
+            state_dict_r2d.keys()) - set(inflated_param_names)
+        if remaining_names:
+            logger.info(f'These parameters in the 2d checkpoint are not loaded'
+                        f': {remaining_names}')
+
     def init_weights(self):
         if isinstance(self.pretrained, str):
             logger = get_root_logger()
             logger.info(f'load model from: {self.pretrained}')
             if self.pretrained2d:
                 # Inflate 2D model into 3D model.
-                self.inflate_weights(logger)
+                self._inflate_weights(logger)
 
             else:
                 load_checkpoint(
